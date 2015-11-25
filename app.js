355a71ec4f2a81058433af8ea73d1b6f5c4c1797
Xueli Zhang <xueli.zhang@carleton.ca>
2015-11-25 18:04:45 -0500

    updates

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var settings = require('./settings');
-var users = require('./routes/users');
 var session = require('express-session');
+var flash = require('connect-flash');
+var users = require('./routes/users');
 var MongoStore = require('connect-mongo')(session);
 
 var app = express();
@@ -16,13 +17,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(flash());
 app.use(favicon(__dirname+'/favicon.ico'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('secret'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+var User = require('../models/user.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '幻想紅樓夢 I--櫻花飛舞時' });
 });
+router.post('/signUp', function(req, res){
+	console.log("get in to signUp");
+	var name = req.body.name,
+	password = req.body.password,
+		email = req.body.email;
 
+	var md5 = crypto.createHash('md5'),
+	password = md5.update(req.body.password).digest('hex');
+	var newUser = new User({
+		name : name,
+		password : password,
+		email : email
+	});
+
+	User.get(newUser.name, function(err, user){
+		if(err){
+			req.flash('signErr', err);
+			return res.redirect('/');
+		}
+		if(user){
+			req.flash('signErr', '玩家名稱已存在！');
+			return res.redirect('/');
+		}
+		if(email){
+			req.flash('signErr', '郵箱已經在本網站使用過！');
+			return res.redirect('/');
+		}
+		newUser.save(function(err, user){
+			if(err){
+				req.flash('signErr', err);
+				return res.redirect('/');
+			}
+			req.session.user = user;
+			console.log('finishing reg');
+			req.flash('success', '註冊成功，歡迎加入！');
+			res.redirect('/');
+		});
+	});
+});
+router.get('/signUp',function(req, res){
+	res.render('index',{title: req.flash()});
+})
 module.exports = router;
